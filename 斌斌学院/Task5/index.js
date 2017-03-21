6f1c2382a79a98bca1c9e07bd99129fd679f7e12
ChunLing <xcl8202@qq.com>
2017-03-21 22:44:12 +0800

    封装函数

--- 斌斌学院/Task5/index.js
+++ 斌斌学院/Task5/index.js
@@ -1,12 +1,12 @@
 window.onload = function () {
 	var input = document.getElementById('input');			// 输入框DOM对象
 	var leftIn = document.getElementById('leftIn');			// 左侧入按钮对象
-	var rightIn = document.getElementById('rightIn');		// 右侧如按钮对象
+	var rightIn = document.getElementById('rightIn');		// 右侧入按钮对象
 	var leftOut = document.getElementById('leftOut')		// 左侧出按钮对象
 	var rightOut = document.getElementById('rightOut');		// 右侧出按钮对象
 	var auto = document.getElementById('auto');				// 自动生成按钮对象
 	var sortBtn = document.getElementById('sortBtn');		// 排序按钮对象
-	var box = document.getElementById('box');				// 第一个显示框对象
+	var box = document.getElementById('box');				// box显示框对象
 	var sort = document.getElementById('sort');				// 排序后显示框对象
 
 	// 用于存放输入框中的值
@@ -20,11 +20,8 @@ window.onload = function () {
 		if (checkNum(data)) {
 			// 已输入个数小于60
 			if (box.children.length < 60) {
-				var span = document.createElement('span');
-				span.style.height = data + 'px';
 				arr.unshift(data);
-				console.log('arr.unshift:' + arr);
-				box.prepend(span);
+				box.prepend(createNode(data));
 			} else {
 				alert('输入的个数超过60，无法再添加啦！删掉些呗~');
 			}
@@ -36,11 +33,8 @@ window.onload = function () {
 		data = input.value;
 		if (checkNum(data)) {
 			if (box.children.length < 60) {
-				var span = document.createElement('span');
-				span.style.height = data + 'px';
 				arr.push(data);
-				console.log('arr.push:' + arr);
-				box.append(span);
+				box.append(createNode(data));
 			} else {
 				alert('输入的个数超过60，无法再添加啦！删掉些呗~');
 			}
@@ -51,7 +45,6 @@ window.onload = function () {
 	leftOut.addEventListener('click', function () {
 		if (box.children.length != 0) {
 			arr.shift();
-			console.log('arr.shift:' + arr);
 			box.removeChild(box.firstChild);
 		} else {
 			alert('当前队列已空！无法出队！');
@@ -63,7 +56,6 @@ window.onload = function () {
 	rightOut.addEventListener('click', function () {
 		if (box.children.length != 0) {
 			arr.pop();
-			console.log('arr.pop:' + arr);
 			box.removeChild(box.lastChild);
 		} else {
 			alert('当前队列已空！无法出队！');
@@ -73,33 +65,21 @@ window.onload = function () {
 	// 点击自动生成
 	auto.addEventListener('click', function () {
 		arr = [];
-		while (sort.hasChildNodes()) {
-			sort.removeChild(sort.firstChild);
-		}
-		while (box.hasChildNodes()) {
-			box.removeChild(box.firstChild);
-		}
+		clearNodes(sort);
+		clearNodes(box);
 		for (var i = 0; i < 60; i++) {
-			var span = document.createElement('span');
 			var rand = parseInt(Math.random() * 90 + 10);
 			arr[i] = rand;
-			span.style.height = rand + 'px';
-			box.append(span);
+			box.append(createNode(rand));
 		}
 	}, false);
 
 	// 点击排序
 	sortBtn.addEventListener('click', function () {
-		while (sort.hasChildNodes()) {
-			sort.removeChild(sort.firstChild);
-		}
-		console.log(arr);
+		clearNodes(sort);
 		var arrSort = sortArr(arr);
-		console.log(arr);
 		for (var i = 0; i < box.children.length; i++) {
-			var span = document.createElement('span');
-			span.style.height = arrSort[i] + 'px';
-			sort.append(span);
+			sort.append(createNode(arrSort[i]));
 		}
 	}, false);
 
@@ -110,13 +90,27 @@ window.onload = function () {
 			input.value = '';
 			return false;
 		} else if (value == '') {
-			alert('请输入数字');
+			alert('请输入数字!');
 			return false;
 		} else if (value >= 100 || value <= 10) {
 			alert('请输入10-100之间的数！');
 			return false;
 		} else {
 			return true;
+		}
+	}
+
+	// 生成节点
+	function createNode(h) {
+		var span = document.createElement('span');
+		span.style.height = h + 'px';
+		return span;
+	}
+
+	// 清除所有子节点
+	function clearNodes(dom) {
+		while (dom.hasChildNodes()) {
+			dom.removeChild(dom.firstChild);
 		}
 	}
 
